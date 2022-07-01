4db140110b505c2d3bdf121ba57493776a90dc7a
l3limp <kakarvardaan@gmail.com>
2022-07-01 13:47:09 +0530

    .

--- lib/src/wallet_connector.dart
+++ lib/src/wallet_connector.dart
@@ -9,6 +9,7 @@ import 'wallet_connect_sdk/session/peer_meta.dart';
 import 'wallet_connect_sdk/walletconnect.dart';
 
 typedef OnSessionUriCallback = void Function(String uri);
+Uri metamaskDownloadLink = Uri.parse("https://metamask.io/download/");
 
 /// WalletConnector is an object for implement WalletConnect protocol for
 /// mobile apps using deep linking to connect with wallets.
@@ -25,23 +26,34 @@ class WalletConnector {
 
   /// Connector using brigde 'https://bridge.walletconnect.org' by default.
   factory WalletConnector(AppInfo? appInfo, {String? bridge}) {
-    final connector = WalletConnect(
-      bridge: bridge ?? 'https://bridge.walletconnect.org',
-      clientMeta: PeerMeta(
-        name: appInfo?.name ?? 'WalletConnect',
-        description: appInfo?.description ?? 'WalletConnect Developer App',
-        url: appInfo?.url ?? 'https://walletconnect.org',
-        icons: appInfo?.icons ??
-            [
-              'https://gblobscdn.gitbook.com/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media'
-            ],
-      ),
-    );
+    var _connector = WalletConnect();
+    try {
+      final connector = WalletConnect(
+        bridge: bridge ?? 'https://bridge.walletconnect.org',
+        clientMeta: PeerMeta(
+          name: appInfo?.name ?? 'WalletConnect',
+          description: appInfo?.description ?? 'WalletConnect Developer App',
+          url: appInfo?.url ?? 'https://walletconnect.org',
+          icons: appInfo?.icons ??
+              [
+                'https://gblobscdn.gitbook.com/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media'
+              ],
+        ),
+      );
+      _connector = connector;
 
-    return WalletConnector._internal(
-      connector: connector,
-      appInfo: appInfo,
-    );
+      return WalletConnector._internal(
+        connector: connector,
+        appInfo: appInfo,
+      );
+    } catch (e) {
+      launchUrl(metamaskDownloadLink);
+      print(e);
+      return WalletConnector._internal(
+        connector: _connector,
+        appInfo: appInfo,
+      );
+    }
   }
 
   /// Get public address, wallet param only use on iOS and using metamask by default
@@ -52,32 +64,31 @@ class WalletConnector {
   /// if user reject session in wallet, or something wrong happen throw an error
   /// in other case throw 'Unexpected exception'
   Future<String> publicAddress({Wallet wallet = Wallet.metamask}) async {
-    Uri metamaskDownloadLink = Uri.parse("https://metamask.io/download/");
     if (!connector.connected) {
       final session = await connector.createSession(
         onDisplayUri: (uri) async {
           var deeplink = DeeplinkHelper.getDeeplink(wallet: wallet, uri: uri);
           if (!await launch(deeplink, forceSafariVC: false)) {
-            throw 'Could not open $deeplink';
+            throw Future.error(Exception("Platformmmmm"));
           }
         },
       ).catchError((onError) {
         launchUrl(metamaskDownloadLink);
-        throw onError;
+        throw Future.error(Exception("Platformmmmm"));
       });
       if (session.accounts.isNotEmpty) {
         var address = session.accounts.first;
         return address;
       } else {
         launchUrl(metamaskDownloadLink);
-        throw 'Unexpected exception';
+        throw Future.error(Exception("Platformmmmm"));
       }
     } else {
       if (connector.session.accounts.isNotEmpty) {
         return connector.session.accounts.first;
       } else {
         launchUrl(metamaskDownloadLink);
-        throw 'Unexpected exception';
+        throw Future.error(Exception("Platformmmmm"));
       }
     }
   }
